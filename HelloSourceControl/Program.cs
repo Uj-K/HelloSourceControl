6cc2a9de21d1d82aa88b787867369321191ee703
Uj-K <umkrall@gmail.com>
2025-01-07 20:10:14 -0800

    Added empty lines between outputs

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -1,9 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, GitHub!");
 
+Console.WriteLine();
+
 for (int i = 1; i <= 10; i++) 
 {
     Console.WriteLine($"Number: {i}");
 }
+
+Console.WriteLine();
 
 Console.WriteLine(DateTime.Now);
